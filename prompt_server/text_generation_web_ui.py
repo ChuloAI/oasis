cf1d4cb2dbbcaaf99207c93bca525b88814d379b
Paolo Rechia <paolorechia@gmail.com>
2023-05-22 21:01:59 +0200

    Add some docstrings using plugin :)

--- prompt_server/text_generation_web_ui.py
+++ prompt_server/text_generation_web_ui.py
@@ -6,6 +6,16 @@ from dataclasses import dataclass
 
 
 def _default_extractor(json_response: Dict[str, Any], stop_parameter_name) -> str:
+    """
+    This function extracts the response from the JSON object using the default parameter name.
+    
+    Parameters:
+        json_response (dict): The JSON response to be extracted.
+        stop_parameter_name (str): The name of the parameter that stops the extraction process.
+        
+    Returns:
+        str: The extracted response.
+    """
     return json_response["response"]
 
 
@@ -75,6 +85,8 @@ def ui_default_parameters():
 
 
 def _response_extractor(json_response, stopping_strings):
+    """Extract relevant information from the given JSON response."""
+
     result = json_response["results"][0]["text"]
     for stop_string in stopping_strings:
         # The stop strings from text-generation-webui come back without the last char
@@ -88,6 +100,19 @@ def _response_extractor(json_response, stopping_strings):
 def build_text_generation_web_ui_client_llm(
     prompt_url="http://0.0.0.0:5000/api/v1/generate", parameters=None
 ):
+    """
+    This function builds a text generation web UI client using LLM (Largue Language Machine) API. 
+    It takes a URL for the LLM server and optional parameters as inputs. 
+    If parameters are not provided, default ones will be used. The function returns an HTTP client that can generate text based on user input.
+    
+    Parameters:
+    prompt_url (str): URL of the LLM server.
+    parameters (Optional[dict]): Optional parameters to pass to the LLM API.
+    
+    Returns:
+    An HTTP client object that can generate text based on user input.
+    """
+        
     if parameters is None:
         parameters = ui_default_parameters()
 
@@ -97,5 +122,4 @@ def build_text_generation_web_ui_client_llm(
         stop_parameter_name="stopping_strings",
         response_extractor=_response_extractor,
     )
-
 
