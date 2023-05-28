418c9a2d41c1a6e54b9b77dd3ce69747f78c087b
Paolo Rechia <paolorechia@gmail.com>
2023-05-28 15:19:28 +0200

    Handling indentation cases

--- test_parser.py
+++ test_parser.py
@@ -1,32 +1,18 @@
 import pytest
-import ast
-
 from prompt_server.parser import function_parser
 
 
-
-def test_no_indent_tab():
-    s = "def hello():\n\tprint('hello')"
-    header, body, leading = function_parser(s)
+@pytest.mark.parametrize(
+    "input_function, expected_leading",
+    [
+        ("def hello():\n\tprint('hello')", ""),
+        ("def hello():\n    print('hello')", ""),
+        ("\tdef hello():\n\t\tprint('hello')", "\t"),
+        ("    def hello():\n        print('hello')", "    "),
+    ]
+)
+def test_different_indentations(input_function, expected_leading):
+    header, body, leading = function_parser(input_function)
     assert header == "def hello():"
-    assert body == "\tprint('hello')"
-    assert leading == ""
-
-# def test_no_indent_spaces():
-#     s = "def hello():\n    print('hello')"
-#     h = function_parser(s)
-
-# def test_indent_tabs():
-#     try:
-#         s = "\tdef hello():\n\t\tprint('hello')"
-#         h2 = function_parser(s)
-#     except IndentationError as excp:
-#         raise excp
-
-# def test_indent_spaces():
-#     try:
-#         s = "    def hello():\n        print('hello')"
-#         h2 = function_parser(s)
-#     except IndentationError as excp:
-#         raise excp
-
+    assert body == expected_leading + "print('hello')"
+    assert leading == expected_leading
